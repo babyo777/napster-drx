0075d38922ad73813dfd217f94f8f642fe838af6
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-20 04:43:34 +0530

    fix

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -2,6 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import musicReducer from "./Player";
 export const store = configureStore({
   reducer: { musicReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredPaths: ["musicReducer.music"],
+        ignoredActions: ["Music/setPlayer"],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
