f57bf579620f3eac991830027b82a6cf703b3475
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-20 03:41:39 +0530

    Update InstallNapster.tsx

--- src/components/InstallNapster.tsx
+++ src/components/InstallNapster.tsx
@@ -1,6 +1,6 @@
 function InstallNapster() {
   return (
-    <div className=" bg-white text-black bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] h-svh space-y-5 flex flex-col p-4 justify-center  items-center">
+    <div className=" bg-white fade-in text-black bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] h-svh space-y-5 flex flex-col p-4 justify-center  items-center">
       <h2 className="text-2xl text-center  mb-2">
         Install Napster on Your Device
       </h2>
