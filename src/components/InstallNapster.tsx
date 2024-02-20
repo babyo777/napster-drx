9d026823172247616c5dfaca2839ccb22a50c9ea
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-20 05:51:30 +0530

    Update InstallNapster.tsx

--- src/components/InstallNapster.tsx
+++ src/components/InstallNapster.tsx
@@ -3,7 +3,7 @@ function InstallNapster() {
   return (
     <div className=" bg-white fade-in text-black bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] min-h-svh space-y-5 flex flex-col p-11 justify-center  items-center">
       <h2 className="text-lg text-center font-semibold mb-2">
-        Install Napster-drx on Your Device or{" "}
+        Install Napster-drx on Your Device Or{" "}
         <a
           href=""
           className="text-red-500 font-normal underline-offset-4 underline"
