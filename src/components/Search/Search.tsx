8e71def5c96e0286a5874dfa7f1374992879d3ee
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-23 20:11:52 +0530

    Update Search.tsx

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -119,7 +119,7 @@ function SearchComp() {
             searchQuery.length <= 0 && (
               <>
                 <h3 className="text-xs text-zinc-500 pt-2 pb-1 ">
-                  Recent Search
+                  Recent Search Played
                 </h3>
                 <div className="flex flex-col space-y-2.5  py-2.5">
                   {RecentSearch.slice(0, 4).map((recentSearch) => (
