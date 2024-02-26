ae1ec4dd5d731d82b5db46a31282b5db416fad70
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-27 00:31:05 +0530

    Update News.tsx

--- src/components/Whats-new/News.tsx
+++ src/components/Whats-new/News.tsx
@@ -1,61 +1,73 @@
 import { Alert, AlertDescription } from "../ui/alert";
 import { Avatar } from "@/components/ui/avatar";
 import { Button } from "../ui/button";
-import { useEffect, useState } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { DATABASE_ID, UPDATES, db } from "@/appwrite/appwriteConfig";
-function News() {
-  const [close, setClose] = useState<boolean>(false);
+import { useQuery } from "react-query";
+import { useState } from "react";
 
-  useEffect(() => {
-    db.getDocument(
+function News() {
+  const [close, setClose] = useState<boolean>(true);
+  const update = async () => {
+    const c = await db.getDocument(
       DATABASE_ID,
       UPDATES,
       localStorage.getItem("uid") || "default"
-    )
-      .then(() => setClose(false))
-      .catch(() => setClose(true));
-  }, []);
-  const handleClose = () => {
+    );
+    return c;
+  };
+  const { isError, refetch } = useQuery("update", update, {
+    refetchOnWindowFocus: false,
+    staleTime: Infinity,
+    retry: 0,
+    refetchOnMount: false,
+  });
+
+  const handleClose = async () => {
     setClose(false);
-    db.createDocument(
+    await db.createDocument(
       DATABASE_ID,
       UPDATES,
       localStorage.getItem("uid") || "default",
       {
         user: localStorage.getItem("uid"),
       }
-    ).then(() => {
-      setClose(false);
-    });
+    );
+    refetch();
   };
   return (
     <>
       {close && (
-        <div className=" fixed w-full px-4">
-          <Alert className=" fade-in bg-zinc-800 rounded-xl  top-5 ">
-            <AlertDescription>
-              <div className="flex items-center justify-between space-x-2">
-                <Avatar className="h-9 w-9">
-                  <LazyLoadImage
-                    effect="blur"
-                    src="https://static-00.iconduck.com/assets.00/spotify-icon-2048x2048-5gqpkwih.png"
-                  />
-                </Avatar>
-                <span className="text-xs">
-                  Soon you will be able to transfer your spotify playlists.
-                </span>
-                <Button
-                  onClick={handleClose}
-                  className="text-xs py-1.5 bg-zinc-600 font-normal hover:bg-zinc-700 text-white px-3 rounded-md h-fit"
-                >
-                  Close
-                </Button>
-              </div>
-            </AlertDescription>
-          </Alert>
-        </div>
+        <>
+          {isError && (
+            <div className=" fixed w-full px-4">
+              <Alert className=" fade-in bg-zinc-800 rounded-xl  top-5 ">
+                <AlertDescription>
+                  <div className="flex items-center justify-between space-x-2">
+                    <Avatar className="h-9 w-9">
+                      <LazyLoadImage
+                        effect="blur"
+                        src="https://static-00.iconduck.com/assets.00/spotify-icon-2048x2048-5gqpkwih.png"
+                      />
+                    </Avatar>
+                    <span className="text-xs">
+                      Soon you will be able to transfer your spotify playlists.
+                    </span>
+                    <Button
+                      onClick={handleClose}
+                      className={`text-xs py-1.5 
+                    bg-zinc-600 
+                   font-normal hover:bg-zinc-700 text-white px-3 rounded-md h-fit`}
+                    >
+                      close
+                    </Button>
+                  </div>
+                </AlertDescription>
+              </Alert>
+            </div>
+          )}
+        </>
       )}
     </>
   );
