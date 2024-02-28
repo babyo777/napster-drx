775e808e662a580a9dadc3aa98008a994f0eb4f2
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-28 20:45:19 +0530

    Update AudioPLayer.tsx

--- src/components/Footer/AudioPLayer.tsx
+++ src/components/Footer/AudioPLayer.tsx
@@ -219,8 +219,6 @@ function AudioPLayerComp() {
     dispatch(setPlayer(sound));
     sound.play();
     return () => {
-      sound.pause();
-      sound.off();
       sound.unload();
       navigator.mediaSession.setActionHandler("play", null);
       navigator.mediaSession.setActionHandler("pause", null);
