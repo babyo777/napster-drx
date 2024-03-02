3a9198ddf3d66f7aed072c73e15f77ce140d4557
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-02 17:07:09 +0530

    Update Loader.tsx

--- src/components/Loaders/Loader.tsx
+++ src/components/Loaders/Loader.tsx
@@ -14,7 +14,7 @@ function Loader({
   return (
     <l-line-spinner
       size={size || "40"}
-      stroke={stroke || "4"}
+      stroke={stroke || "3"}
       speed="1"
       color={color || "gray"}
     ></l-line-spinner>
