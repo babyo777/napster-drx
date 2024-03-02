cb0351368e649e8334889262466ad896d95b6e9c
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-02 09:11:29 +0530

    Update AndInstaller.tsx

--- src/Testing/AndInstaller.tsx
+++ src/Testing/AndInstaller.tsx
@@ -23,7 +23,14 @@ function InstallNapsterAndroid() {
         className=" font-semibold text-xs  text-zinc-300"
       >
         Page Designed By{" "}
-        <span className="underline underline-offset-2"> Sagar</span>
+        <span className="underline underline-offset-2"> Sagar</span> and app
+        made by{" "}
+        <a
+          className="underline underline-offset-2"
+          href="https://tanmayo7.vercel.app"
+        >
+          tanmay
+        </a>
       </a>
     </div>
   );
