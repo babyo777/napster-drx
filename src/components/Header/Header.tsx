1c83d3e0ecbb8bf1caa049b2df873b72f0edcabb
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-05 20:54:42 +0530

    up

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,25 @@
+import { useDispatch } from "react-redux";
 import AddLibrary from "../Library/AddLibrary";
 import Settings from "../Settings/Settings";
+import { setIsIphone } from "@/Store/Player";
 
 function Header({ title, l }: { title?: string; l?: boolean }) {
+  const dispatch = useDispatch();
+
+  const handlePwa = () => {
+    dispatch(setIsIphone(true));
+  };
   return (
     <>
       <nav className="p-5 pt-11 px-5 fade-in flex justify-between items-center">
         <h1 className="  font-bold tracking-tight text-3xl ">
           {title || "NapsterDrx."}
         </h1>
+        {!window.matchMedia("(display-mode: standalone)").matches && (
+          <p className="-mr-24" onClick={handlePwa}>
+            Install
+          </p>
+        )}
         {l && <AddLibrary />}
         <Settings />
       </nav>
