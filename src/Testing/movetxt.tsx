baa3e809dcff8c5d11d0cfbeeaf904f61bb71d6e
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-05 21:47:07 +0530

    u

--- src/Testing/movetxt.tsx
+++ src/Testing/movetxt.tsx
@@ -30,7 +30,7 @@ const CarouselSlide = ({ image, text }: CarouselSlideProps) => (
   </div>
 );
 
-export function AndroidScreenshots() {
+export function Moved() {
   const plugin = React.useRef(
     Autoplay({ delay: 2000, stopOnInteraction: true })
   );
@@ -44,14 +44,20 @@ export function AndroidScreenshots() {
     >
       <CarouselContent className="rounded-xl">
         <CarouselItem className="rounded-xl">
-          <CarouselSlide image="https://cdn.discordapp.com/attachments/1213096773342601246/1213096773829001256/RPReplay_Final1709229551.mov?ex=65f43b69&is=65e1c669&hm=8b85aa134b7fb96f2fbae629294c2876ca6cc7e6da108edb5aacde51eb869437&" text="Slide 1" />
+          <CarouselSlide
+            image="https://cdn.discordapp.com/attachments/1213096773342601246/1213096773829001256/RPReplay_Final1709229551.mov?ex=65f43b69&is=65e1c669&hm=8b85aa134b7fb96f2fbae629294c2876ca6cc7e6da108edb5aacde51eb869437&"
+            text="Slide 1"
+          />
         </CarouselItem>
         <CarouselItem className="rounded-xl">
           <CarouselSlide image="/ui/Andriod/cropped-tele1.jpg" text="Slide 2" />
         </CarouselItem>
 
         <CarouselItem className="rounded-xl">
-          <CarouselSlide image="/ui/Andriod/cropped-tele 2.jpg" text="Slide 3" />
+          <CarouselSlide
+            image="/ui/Andriod/cropped-tele 2.jpg"
+            text="Slide 3"
+          />
         </CarouselItem>
         <CarouselItem className="rounded-xl">
           <CarouselSlide image="/ui/Andriod/cropped-tele3.jpg" text="Slide 4" />
@@ -65,3 +71,7 @@ export function AndroidScreenshots() {
         <CarouselItem className="rounded-xl">
           <CarouselSlide image="/ui/Andriod/cropped-and4.jpg" text="Slide 7" />
         </CarouselItem>
+      </CarouselContent>
+    </Carousel>
+  );
+}
