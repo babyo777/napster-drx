b01d6df852718cbec8ac8d98ac801cdbc8f09cc6
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-05 13:48:05 +0530

    Update 404.tsx

--- src/components/404.tsx
+++ src/components/404.tsx
@@ -6,7 +6,7 @@ function NotFound() {
     <div className=" relative  w-full">
       <div className="fixed  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <Link to={"/search"} className=" text-zinc-300 underline">
-          Not found
+          Search
         </Link>
       </div>
       <GoBack />
