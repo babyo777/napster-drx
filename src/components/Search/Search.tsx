2f3f6d39de4c90381d1ee1b11f28004849ea018d
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-11 14:04:04 +0530

    Update Search.tsx

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -200,7 +200,7 @@ function SearchComp() {
           {searchQuery.length == 0 && (
             <>
               {RecentLoading && (
-                <div className="flex fade-in flex-col space-y-1 ">
+                <div className="flex fade-in flex-col space-y-2 ">
                   <SkeletonP />
                   <SkeletonP />
                   <SkeletonP />
