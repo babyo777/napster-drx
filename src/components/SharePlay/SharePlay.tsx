d18b3c6b1b287e77d7907957ac2130687d9d71ab
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-14 19:58:01 +0530

    Update SharePlay.tsx

--- src/components/SharePlay/SharePlay.tsx
+++ src/components/SharePlay/SharePlay.tsx
@@ -6,7 +6,7 @@ function SharePlay() {
     <>
       <Header title="Share Play" />
       <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center space-y-2">
-        <Loader />
+        <Loader loading={true} />
         <span className="text-zinc-400 text-sm">Coming soon...</span>
       </div>
     </>
