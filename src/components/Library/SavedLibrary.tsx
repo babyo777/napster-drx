e91a832059ea79898aaeb9dc3520a2964e9c2e1c
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-14 17:17:29 +0530

    u

--- src/components/Library/SavedLibrary.tsx
+++ src/components/Library/SavedLibrary.tsx
@@ -104,7 +104,7 @@ function SavedLibraryComp() {
         <Link to={`/liked/${localStorage.getItem("uid")}`}>
           <div className="flex space-x-2 px-5 mb-3 items-center justify-between">
             <div className="flex items-center space-x-2">
-              <div className="overflow-hidden h-[3.4rem]  w-[3.4rem] ">
+              <div className="overflow-hidden h-14  w-14 ">
                 <AspectRatio ratio={1 / 1}>
                   <LazyLoadImage
                     height="100%"
