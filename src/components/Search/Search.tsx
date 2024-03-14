e7d90cb490b1ab62ed9b36b3d5674df7061b2afe
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-14 17:27:56 +0530

    Update Search.tsx

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -41,7 +41,7 @@ function SearchComp() {
     const r = await db.listDocuments(DATABASE_ID, INSIGHTS, [
       Query.orderDesc("$createdAt"),
       Query.equal("for", [localStorage.getItem("uid") || ""]),
-      Query.limit(7),
+      Query.limit(11),
     ]);
     const p = r.documents as unknown as likedSongs[];
     return p;
