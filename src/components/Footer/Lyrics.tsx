47fef1af101a8d483782c706b919760780319f78
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-17 20:42:45 +0530

    Update Lyrics.tsx

--- src/components/Footer/Lyrics.tsx
+++ src/components/Footer/Lyrics.tsx
@@ -46,7 +46,6 @@ function Lyrics({ closeRef }: { closeRef: RefObject<HTMLButtonElement> }) {
       refetchOnWindowFocus: false,
       staleTime: 60 * 6000,
       refetchOnMount: false,
-      retry: 5,
     }
   );
   useEffect(() => {
