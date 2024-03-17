ad17e96454ae617215d7fcc14dbe5e9c103458fd
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-17 09:15:40 +0530

    Update AudioPLayer.tsx

--- src/components/Footer/AudioPLayer.tsx
+++ src/components/Footer/AudioPLayer.tsx
@@ -532,7 +532,7 @@ function AudioPLayerComp() {
                 <div className="flex items-center justify-between w-full">
                   <TfiLoop
                     className={`h-6 w-6 ${
-                      music && music.loop ? "text-white" : "text-zinc-black"
+                      music && music.loop ? "text-white" : "text-zinc-500"
                     }`}
                     onClick={handleLoop}
                   />
