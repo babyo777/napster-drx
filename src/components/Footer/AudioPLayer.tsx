ba3aa5f56f8155e2f0473fc5c56da0cf4794bd3b
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-17 13:39:44 +0530

    Update AudioPLayer.tsx

--- src/components/Footer/AudioPLayer.tsx
+++ src/components/Footer/AudioPLayer.tsx
@@ -443,14 +443,14 @@ function AudioPLayerComp() {
                       }`}
                     >
                       <DrawerClose className="text-start">
-                        <p className="text-base truncate fade-in  w-[70vw] text-red-500">
+                        <p className="text-base truncate transition-all duration-300 fade-in  w-[70vw] text-red-500">
                           {" "}
                           {playlist[currentIndex].artists[0]?.name}
                         </p>
                       </DrawerClose>
                     </Link>
                   ) : (
-                    <p className="text-base truncate fade-in  w-64 text-red-500">
+                    <p className="text-base truncate transition-all duration-300 fade-in  w-64 text-red-500">
                       {" "}
                       Unknown
                     </p>
