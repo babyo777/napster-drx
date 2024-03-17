0165b3ff7bfb0f87fdc9a46f7655107cc5742e5b
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-17 10:11:17 +0530

    u

--- src/App.tsx
+++ src/App.tsx
@@ -27,12 +27,6 @@ function AppComp() {
         console.log(error);
       }
     }
-    const themeColorMetaTag = document.querySelector(
-      'meta[name="theme-color"]'
-    );
-    if (themeColorMetaTag) {
-      themeColorMetaTag.setAttribute("content", "#fffff");
-    }
   }, [token]);
 
   const handleTester = async () => {
