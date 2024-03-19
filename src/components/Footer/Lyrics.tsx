8d599a0874afee44aef8f1f61ef3f1685824ab21
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-19 22:51:13 +0530

    Update Lyrics.tsx

--- src/components/Footer/Lyrics.tsx
+++ src/components/Footer/Lyrics.tsx
@@ -43,15 +43,15 @@ function Lyrics({ closeRef }: { closeRef: RefObject<HTMLButtonElement> }) {
 
   const getColor = useCallback(async () => {
     const c = await prominent(playlist[currentIndex].thumbnailUrl, {
-      amount: 21,
+      amount: 11,
       format: "hex",
     });
 
-    if (c[19] === "#000000" || containsDarkColor(c[19] as string)) {
+    if (c[10] === "#000000" || containsDarkColor(c[10] as string)) {
       return setColor(null);
     }
 
-    setColor(c[19] as string);
+    setColor(c[10] as string);
   }, [playlist, currentIndex]);
 
   function containsDarkColor(color: string) {
