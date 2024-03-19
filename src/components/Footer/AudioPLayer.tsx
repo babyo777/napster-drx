f061635453d8a0c1d3cd5de3b8b6e4a60738a742
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-19 18:50:55 +0530

    Update AudioPLayer.tsx

--- src/components/Footer/AudioPLayer.tsx
+++ src/components/Footer/AudioPLayer.tsx
@@ -238,6 +238,25 @@ function AudioPLayerComp() {
         if (isLooped) {
           sound.loop = true;
         }
+        navigator.mediaSession.metadata = new MediaMetadata({
+          title: playlist[currentIndex].title,
+          artist: playlist[currentIndex].artists[0]?.name,
+          album: "",
+          artwork: [
+            {
+              src: playlist[currentIndex].thumbnailUrl.replace(
+                "w120-h120",
+                "w1080-h1080"
+              ),
+            },
+          ],
+        });
+
+        navigator.mediaSession.setActionHandler("play", () => sound.play());
+        navigator.mediaSession.setActionHandler("pause", () => sound.pause());
+        navigator.mediaSession.setActionHandler("nexttrack", handleNext);
+        navigator.mediaSession.setActionHandler("previoustrack", handlePrev);
+        navigator.mediaSession.setActionHandler("seekto", handleSeek);
         dispatch(play(true));
         saveLastPlayed();
       };
@@ -264,25 +283,6 @@ function AudioPLayerComp() {
       };
 
       const handleLoad = () => {
-        navigator.mediaSession.metadata = new MediaMetadata({
-          title: playlist[currentIndex].title,
-          artist: playlist[currentIndex].artists[0]?.name,
-          album: "",
-          artwork: [
-            {
-              src: playlist[currentIndex].thumbnailUrl.replace(
-                "w120-h120",
-                "w1080-h1080"
-              ),
-            },
-          ],
-        });
-
-        navigator.mediaSession.setActionHandler("play", () => sound.play());
-        navigator.mediaSession.setActionHandler("pause", () => sound.pause());
-        navigator.mediaSession.setActionHandler("nexttrack", handleNext);
-        navigator.mediaSession.setActionHandler("previoustrack", handlePrev);
-        navigator.mediaSession.setActionHandler("seekto", handleSeek);
         dispatch(setIsLoading(false));
         setDuration(sound.duration);
         setProgress(sound.currentTime);
