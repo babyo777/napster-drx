4ebc227be99ea72f16053a395c4aec71e6749e85
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-19 16:32:27 +0530

    Update Lyrics.tsx

--- src/components/Footer/Lyrics.tsx
+++ src/components/Footer/Lyrics.tsx
@@ -48,20 +48,17 @@ function Lyrics({ closeRef }: { closeRef: RefObject<HTMLButtonElement> }) {
     if (c === "#000000") {
       return setColor(null);
     }
+
     setColor(color as string);
   }, [playlist, currentIndex, color]);
   const formatDuration = useCallback((seconds: number | "--:--") => {
-    if (seconds === "--:--") return seconds;
-
+    if (seconds == "--:--") return seconds;
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = Math.floor(seconds % 60);
-    const milliseconds = Math.floor((seconds % 1) * 100);
 
     const formattedMinutes = String(minutes).padStart(2, "0");
     const formattedSeconds = String(remainingSeconds).padStart(2, "0");
-    const formattedMilliseconds = String(milliseconds).padStart(2, "0");
-
-    return `[${formattedMinutes}:${formattedSeconds}.${formattedMilliseconds}]`;
+    return `${formattedMinutes}:${formattedSeconds}`;
   }, []);
   const getLyrics = useCallback(async () => {
     // console.log(
