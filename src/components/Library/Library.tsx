f37f16272b3c120afc3279567843c8926d030500
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-23 10:02:30 +0530

    u

--- src/components/Library/Library.tsx
+++ src/components/Library/Library.tsx
@@ -264,7 +264,7 @@ function LibraryComp() {
       )}
       {data && (
         <>
-          <div className="flex w-screen h-[25rem] pt-[5vh] justify-center  relative ">
+          <div className="flex w-screen h-[25rem] pt-[5.3vh] justify-center  relative ">
             <GoBack />
 
             <div className="absolute top-4 z-10 right-3  flex-col space-y-0.5">
