7d7cdfbdcfce27ef2fb0f98b5a89af2aa8d9d602
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-23 19:34:55 +0530

    Update Lyrics.tsx

--- src/components/Footer/Lyrics.tsx
+++ src/components/Footer/Lyrics.tsx
@@ -270,8 +270,8 @@ function Lyrics({ closeRef }: { closeRef: RefObject<HTMLButtonElement> }) {
                           (line.time <= progress &&
                             (index === lyrics.length - 1 ||
                               (lyrics[index + 1]?.time || 0) > progress) &&
-                            "3rem") ||
-                          "3rem",
+                            "2.7rem") ||
+                          "2.7rem",
 
                         transitionProperty: "all",
                         transitionDuration: "800ms",
