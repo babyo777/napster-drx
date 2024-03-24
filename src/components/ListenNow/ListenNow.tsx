4d97471b1523ad1b0f567eb89cdd697d1a2afcca
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-24 17:57:36 +0530

    Update ListenNow.tsx

--- src/components/ListenNow/ListenNow.tsx
+++ src/components/ListenNow/ListenNow.tsx
@@ -136,18 +136,12 @@ export function ListenNowComp() {
       {!chart && !artist && !suggested && (
         <>
           <div className="flex px-4  space-x-4 items-center w-full mt-3">
-            <Skeleton className="w-[40vw] h-4 rounded-md bg-zinc-500" />
+            <Skeleton className="w-[37vw] h-4 rounded-md bg-zinc-500" />
           </div>
           <div className="flex px-4 justify-center space-x-4 items-center w-full mt-5">
-            <Skeleton className="w-[50vw] h-36 rounded-md bg-zinc-500" />
-            <Skeleton className="w-[50vw] h-36 rounded-md bg-zinc-500" />
-          </div>
-          <div className="flex px-4  space-x-4 items-center w-full mt-5">
-            <Skeleton className="w-[24vw] h-4 rounded-md bg-zinc-500" />
-          </div>
-          <div className="flex px-4 justify-center space-x-4 items-center w-full mt-5">
-            <Skeleton className="w-[50vw] h-36 rounded-md bg-zinc-500" />
-            <Skeleton className="w-[50vw] h-36 rounded-md bg-zinc-500" />
+            <Skeleton className="w-[30vw] h-40 rounded-md bg-zinc-500" />
+            <Skeleton className="w-[30vw] h-40 rounded-md bg-zinc-500" />
+            <Skeleton className="w-[30vw] h-40 rounded-md bg-zinc-500" />
           </div>
           <div className="flex px-4  space-x-4 items-center w-full mt-5">
             <Skeleton className="w-[27vw] h-4 rounded-md bg-zinc-500" />
@@ -156,6 +150,15 @@ export function ListenNowComp() {
             <Skeleton className="w-20 h-20 rounded-full bg-zinc-500" />
             <Skeleton className="w-20 h-20 rounded-full bg-zinc-500" />
             <Skeleton className="w-20 h-20 rounded-full bg-zinc-500" />
+            <Skeleton className="w-20 h-20 rounded-full bg-zinc-500" />
+          </div>
+          <div className="flex px-4  space-x-4 items-center w-full mt-5">
+            <Skeleton className="w-[23vw] h-4 rounded-md bg-zinc-500" />
+          </div>
+          <div className="flex px-4 justify-center space-x-4 items-center w-full mt-5">
+            <Skeleton className="w-[50vw] h-28 rounded-md bg-zinc-500" />
+            <Skeleton className="w-[50vw] h-28 rounded-md bg-zinc-500" />
+            <Skeleton className="w-[50vw] h-28 rounded-md bg-zinc-500" />
           </div>
         </>
       )}
