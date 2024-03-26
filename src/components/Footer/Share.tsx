3141622629e907638c30d5619e38f026ad6d7bad
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-26 13:43:38 +0530

    Update Share.tsx

--- src/components/Footer/Share.tsx
+++ src/components/Footer/Share.tsx
@@ -100,7 +100,7 @@ function ShareLyrics({
         setBlur((prev) => !prev);
         setRound(true);
         return setBlurHash(
-          encode(imageData.data, imageData.width, imageData.height, 3, 3)
+          encode(imageData.data, imageData.width, imageData.height, 3, 4)
         );
       }
     },
