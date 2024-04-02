2de2e773d5ea63dbecdefc903565383a747a0a73
babyo77 <onlybabyyo7@gmail.com>
2024-04-02 21:31:35 +0530

    Update EditInfo.tsx

--- src/components/Library/EditInfo.tsx
+++ src/components/Library/EditInfo.tsx
@@ -1,11 +1,3 @@
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import { Button } from "../ui/button";
 import { useCallback, useRef } from "react";
 
@@ -17,6 +9,14 @@ import {
 import { useQueryClient } from "react-query";
 import { savedPlaylist } from "@/Interface";
 import { RxCross2 } from "react-icons/rx";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
+import { AlertTitle } from "../ui/alert";
 
 const EditInfo: React.FC<{ id: string; f: string; collection?: string }> = ({
   id,
@@ -40,40 +40,42 @@ const EditInfo: React.FC<{ id: string; f: string; collection?: string }> = ({
   }, [id, collection, q]);
 
   return (
-    <Dialog>
-      <DialogTrigger className="flex w-full items-center justify-end space-x-2">
+    <AlertDialog>
+      <AlertDialogTrigger className="flex w-full items-center justify-end space-x-2">
         <RxCross2 className="h-6 w-6 text-zinc-500" />
-      </DialogTrigger>
-      <DialogContent className="items-center rounded-2xl flex flex-col w-[60vw]">
-        <DialogHeader>
-          <DialogTitle className="text-zinc-400 text-sm font-bold">
+      </AlertDialogTrigger>
+      <AlertDialogContent className="items-center m-0 p-0 pt-3 pb-2 border-none rounded-2xl flex flex-col w-[60vw] bg-neutral-900/95">
+        <AlertDialogHeader>
+          <AlertTitle className="text-zinc-300  font-normal pt-1">
             Are you sure?
-          </DialogTitle>
-        </DialogHeader>
+          </AlertTitle>
+        </AlertDialogHeader>
 
-        <div className="flex items-center justify-center space-x-2 ">
+        <div className="flex items-center flex-col w-full justify-center ">
+          <div className=" h-[0.11rem] bg-zinc-800/30 w-full"></div>
           <Button
+            asChild
             disabled={f === "default" ? true : false}
             variant={"secondary"}
             onClick={handleDelete}
-            className="px-7 rounded-xl bg-red-500 "
+            className="px-7 py-5 bg-transparent text-lg font-normal rounded-xl  text-red-500 "
           >
-            Yes
+            <p>Yes</p>
           </Button>
-
-          <DialogClose>
+          <div className=" h-[0.11rem] bg-zinc-800/30 w-full"></div>
+          <AlertDialogCancel className=" bg-transparent  border-none p-0 m-0">
             <Button
               asChild
               ref={closeRef}
               variant={"secondary"}
-              className="px-7 rounded-xl "
+              className="px-7 bg-transparent text-zinc-400 font-normal text-base rounded-xl "
             >
-              <p>No</p>
+              <p className="font-normal">No</p>
             </Button>
-          </DialogClose>
+          </AlertDialogCancel>
         </div>
-      </DialogContent>
-    </Dialog>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
