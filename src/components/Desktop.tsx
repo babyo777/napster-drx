983f4b7f9a9f63759e899a4034a3c949017c0c61
babyo77 <onlybabyyo7@gmail.com>
2024-04-07 04:08:58 +0530

    Update Desktop.tsx

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -12,10 +12,12 @@ function Desktop() {
   };
   return (
     <>
-      <div className="animate-fade-up">
+      <div className=" fade-in">
         <header className=" px-11 max-md:px-5 py-4 bg-transparent fixed w-full backdrop-blur-xl justify-between flex">
-          <h1 className=" font-semibold text-2xl max-md:text-xl">Napster</h1>
-          <ul className=" flex text-lg text-zinc-200  items-center space-x-3">
+          <h1 className=" font-semibold text-2xl max-md:text-xl animate-fade-right">
+            Napster
+          </h1>
+          <ul className=" flex text-lg animate-fade-left text-zinc-200  items-center space-x-3">
             <a
               href="https://github.com/babyo77/napsterDrx-Public"
               target="_blank"
@@ -28,7 +30,7 @@ function Desktop() {
           </ul>
         </header>
 
-        <div className="bg-[#09090B] font-semibold w-full min-h-screen flex justify-center items-center px-9 max-lg:text-4xl max-md:text-4xl text-7xl flex-col space-y-7 text-center max-md:px-4 overflow-hidden fixed -z-10">
+        <div className="bg-[#09090B] font-semibold w-full min-h-screen flex justify-center items-center px-9 max-lg:text-4xl max-md:text-4xl text-7xl flex-col space-y-7 text-center max-md:px-4 animate-fade-up overflow-hidden fixed -z-10">
           <div>
             <p className="">Enjoy Music Without Interruptions</p>
           </div>
@@ -44,7 +46,7 @@ function Desktop() {
             </Button>
           </div>
         </div>
-        <footer className=" fixed text-zinc-400 hover:text-white transition-all duration-300  bottom-0 flex justify-center items-center w-full py-2.5 text-xs space-x-1">
+        <footer className=" animate-fade-up fixed text-zinc-400 hover:text-white transition-all duration-300  bottom-0 flex justify-center items-center w-full py-2.5 text-xs space-x-1">
           <SiGithubsponsors />
           <a
             className=" font-semibold"
