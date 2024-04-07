e2eef40c24aaca1aac18e81815eff2ace4170e66
babyo77 <onlybabyyo7@gmail.com>
2024-04-07 13:10:15 +0530

    Update TransferHrader.tsx

--- src/TransferHrader.tsx
+++ src/TransferHrader.tsx
@@ -1,24 +1,30 @@
-import ProgressBar from "@ramonak/react-progress-bar"
+import ProgressBar from "@ramonak/react-progress-bar";
+import { spotifyTransfer } from "./Interface";
 
-export default function TransferHeader({progress,data}:{progress:number,data:}) {
+export default function TransferHeader({
+  progress,
+  data,
+}: {
+  progress: number;
+  data: spotifyTransfer;
+}) {
   return (
     <div className="flex w-full flex-col space-y-3 items-center">
-              <p className="text-zinc-300  font-semibold text-xl animate-fade-down">
-                {Math.floor((progress / data.tracks.length) * 100)}%
-              </p>
+      <p className="text-zinc-300  font-semibold text-xl animate-fade-down">
+        {Math.floor((progress / data.tracks.length) * 100)}%
+      </p>
 
-              <ProgressBar
-                className=" w-full border-none"
-                height="4px"
-                isLabelVisible={false}
-                bgColor="#1DD45F"
-                maxCompleted={data.tracks.length}
-                completed={progress || 0}
-              />
-              <p className="text-zinc-300  font-semibold text-lg animate-fade-up">
-                Transferred {progress}/{data.tracks.length}
-              </p>
-            </div>
-  )
+      <ProgressBar
+        className=" w-full border-none"
+        height="4px"
+        isLabelVisible={false}
+        bgColor="#1DD45F"
+        maxCompleted={data.tracks.length}
+        completed={progress || 0}
+      />
+      <p className="text-zinc-300  font-semibold text-lg animate-fade-up">
+        Transferred {progress}/{data.tracks.length}
+      </p>
+    </div>
+  );
 }
-
