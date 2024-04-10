95cec010059bec615068e0fb68726f967e3c94e8
babyo77 <onlybabyyo7@gmail.com>
2024-04-10 09:57:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
         theme_color: "#121212",
       },
       workbox: {
-        globPatterns: ["**/*.{js,css,html,jpeg,jpg,json}"],
+        globPatterns: ["**/*.{js,css,html,jpeg,jpg,json,webp}"],
       },
     }),
   ],
