0aca61c0a3fdbea49b084fdadb5462c88092f9b4
babyo77 <onlybabyyo7@gmail.com>
2024-04-14 12:43:52 +0530

    Update SharePlay.tsx

--- src/components/SharePlay/SharePlay.tsx
+++ src/components/SharePlay/SharePlay.tsx
@@ -500,7 +500,7 @@ function SharePlay() {
           )}
         </div>
 
-        {!dbClick && (
+        {dbClick && (
           <div className=" z-10  pb-[5dvh] absolute w-full h-full flex justify-center items-center text-9xl bg-gradient-to-r from-rose-400 to-red-500 bg-clip-text  ">
             <Lottie animationData={likeData} className="h-80 w-80" />
           </div>
