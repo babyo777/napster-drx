a1f29536f2989db8a12ebc7e2b2dab09ab98dc72
babyo77 <onlybabyyo7@gmail.com>
2024-04-14 19:40:25 +0530

    Update Token.tsx

--- src/components/Token.tsx
+++ src/components/Token.tsx
@@ -1,9 +1,5 @@
-import { CopyIcon } from "@radix-ui/react-icons";
-
 import { Button } from "@/components/ui/button";
 
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { useCallback } from "react";
 import {
   Drawer,
