bbb5cc8e2954b25d8e802af5e7978101648c986b
babyo77 <onlybabyyo7@gmail.com>
2024-04-20 11:05:28 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -26,13 +26,13 @@ import ErrorElement from "./error.tsx";
 import User from "./user/User.tsx";
 import Track from "./Track/Track.tsx";
 import Playlists from "./user/Playlists.tsx";
-import Auth from "./Auth.tsx";
+import Check from "./components/Check.tsx";
 
 const client = new QueryClient();
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Auth />,
+    element: <Check />,
     errorElement: <ErrorElement />,
     children: [
       {
