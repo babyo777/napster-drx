e3bc31856589d6337ad8a66cfe8d8716ac2ec8fd
babyo77 <onlybabyyo7@gmail.com>
2024-04-21 18:04:20 +0530

    Update Mode.tsx

--- src/Mode.tsx
+++ src/Mode.tsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import Auth from "./Auth";
 import Offline from "./Offline/offline";
+import { useEffect } from "react";
 
 function Mode() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (navigator.onLine) {
+      navigate("/offline/");
+    }
+  });
   if (navigator.onLine) {
     return <Auth />;
   }
